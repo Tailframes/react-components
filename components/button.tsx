ccf906d193c9aa6694bc21331193c244383b4c70
Tomasz Guzik <tomasz.guzik94@gmail.com>
2024-04-25 23:19:27 +0200

    feat(components/button): update outlined button styles

--- components/button.tsx
+++ components/button.tsx
@@ -10,7 +10,8 @@ const buttonVariants = cva(
       variant: {
         primary: 'bg-blue-700 stroke-white px-6 text-white hover:bg-blue-950',
         secondary: 'bg-blue-50 stroke-blue-700 px-6 text-blue-700 hover:bg-blue-100',
-        outlined: 'border border-blue-700 bg-transparent stroke-blue-700 px-6 text-blue-700 hover:bg-blue-50',
+        outlined:
+          'border border-slate-200 bg-transparent stroke-black px-6 text-black hover:border-blue-700 hover:bg-blue-700 hover:stroke-white hover:text-white',
         text: 'stroke-blue-700 px-2 text-blue-700',
         'text-default': 'stroke-slate-500 px-2 text-black',
       },
