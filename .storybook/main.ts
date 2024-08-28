c8d9498dbd41a93c83c906ed1592b7f45d4dfa5e
Tomasz Guzik <tomasz.guzik94@gmail.com>
2024-08-28 23:21:02 +0200

    build(storybook): remove @storybook/addon-interactions

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
