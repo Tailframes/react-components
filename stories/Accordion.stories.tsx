337b23809283e8144de5da7a007404b0081c2ce7
Tomasz Guzik <tomasz.guzik94@gmail.com>
2024-09-09 23:32:02 +0200

    feat(accordion): update Accordion component with new icons and content

--- stories/Accordion.stories.tsx
+++ stories/Accordion.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import type { ComponentType } from 'react';
-import { UserIcon } from '../assets/user-icon';
-import { Accordion } from '../components/accordion/accordion';
+import { CircleArrowRightIcon } from '../assets/circle-arrow-right-icon';
+import { Accordion, type AccordionProps } from '../components/accordion/accordion';
 import { AccordionItem } from '../components/accordion/accordion-item';
 
 const meta = {
@@ -22,26 +22,86 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const items = [
+const items: AccordionProps['items'] = [
   {
-    label: 'First Label',
-    content:
-      'Feel free to unleash your imagination and let your words flow without inhibition. There are no constraints here; only the boundless potential of your ideas awaits.',
+    label: 'What is Tailframes?',
+    contentClassName: 'text-justify',
+    content: (
+      <p>
+        Tailframes is a comprehensive library of pre-built UI components designed with <strong>React</strong> and{' '}
+        <strong>Tailwind CSS</strong>. Our goal is to help developers quickly and efficiently build beautiful,
+        responsive websites by providing a wide range of customizable <strong>Tailwind CSS components</strong> that
+        adhere to modern <strong>UI/UX design principles</strong>. Whether you&apos;re building a new project from
+        scratch or enhancing an existing one, Tailframes offers the perfect blend of <strong>React components</strong>{' '}
+        to streamline your development process.
+      </p>
+    ),
   },
   {
-    label: 'Second Label',
-    content:
-      'Feel free to unleash your imagination and let your words flow without inhibition. There are no constraints here; only the boundless potential of your ideas awaits. ' +
-      'Feel free to unleash your imagination and let your words flow without inhibition. There are no constraints here; only the boundless potential of your ideas awaits.',
+    label: 'How do I integrate Tailframes components into my project?',
+    content: (
+      <>
+        <p>
+          Integrating <strong>Tailframes</strong> components into your project is straightforward. First, ensure your
+          project is set up with <strong>React</strong> and <strong>Tailwind CSS</strong>. Then, browse through our
+          extensive <strong>Tailwind CSS component library</strong>, select the components you need, and simply copy the
+          provided JSX code. Paste this code into your React components, and customize the styles using Tailwind’s
+          utility classes to match your project’s design specifications.
+        </p>
+        <br />
+        <p>
+          For more detailed instructions, you can refer to our{' '}
+          <a
+            href='https://tailframes.com/getting-started'
+            className='text-blue-500 hover:underline'
+            target='_blank'
+            rel='noreferrer'
+          >
+            documentation
+          </a>{' '}
+          and <strong>Tailwind CSS code examples</strong>, which includes step-by-step guides and examples on using{' '}
+          <strong>React with Tailwind CSS</strong> effectively.
+        </p>
+      </>
+    ),
   },
   {
-    label: 'Third Label',
-    content:
-      'Feel free to unleash your imagination and let your words flow without inhibition. There are no constraints here; only the boundless potential of your ideas awaits. ' +
-      'Feel free to unleash your imagination and let your words flow without inhibition. There are no constraints here; only the boundless potential of your ideas awaits. ' +
-      'Feel free to unleash your imagination and let your words flow without inhibition. There are no constraints here; only the boundless potential of your ideas awaits.',
+    label: 'What are the benefits of using Tailframes?',
+    content: (
+      <>
+        <p className='mb-1'>
+          <strong>Tailframes</strong> offers numerous benefits for developers looking to accelerate their{' '}
+          <strong>web development</strong> process using <strong>React</strong> and <strong>Tailwind CSS</strong>
+        </p>
+        <ul className='ml-4 list-inside list-disc space-y-0.5'>
+          <li>
+            <strong>Speed:</strong> Quickly assemble web pages with pre-built <strong>UI components</strong>, saving
+            significant time on design and implementation.
+          </li>
+          <li>
+            <strong>Customization:</strong> Tailframes components are built with <strong>Tailwind CSS</strong>, allowing
+            easy customization through Tailwind’s utility-first approach.
+          </li>
+          <li>
+            <strong>Responsive Design:</strong> All components are responsive by default, ensuring your{' '}
+            <strong>React applications</strong> look great on any device.
+          </li>
+          <li>
+            <strong>Consistency:</strong> Maintain a consistent design language across your project with components that
+            adhere to modern <strong>UI/UX standards</strong>.
+          </li>
+        </ul>
+        <br />
+        <p>
+          Additionally, Tailframes is constantly updated with new <strong>React components</strong> and features, so you
+          can stay ahead of the curve with the latest UI trends in the <strong>React and Tailwind CSS ecosystem</strong>
+          .
+        </p>
+      </>
+    ),
   },
 ];
+
 export const Text: Story = {
   args: {
     items,
@@ -50,7 +110,7 @@ export const Text: Story = {
 
 export const TextWithStartAdornment: Story = {
   args: {
-    items: items.map(item => ({ ...item, labelStartAdornment: <UserIcon /> })),
+    items: items.map(item => ({ ...item, labelStartAdornment: <CircleArrowRightIcon /> })),
   },
 };
 
